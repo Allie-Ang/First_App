39290963371285632b35abd98f9a2ca5c2e9ccb8
Allison Ang <aang@MacBook-Pro.local>
2022-11-01 18:55:06 -0700

    message

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         PROGRAMMING IS SIIIIIIICK
+         PROGRAMMING IS SIIIICK
     
         </p>
         <a
